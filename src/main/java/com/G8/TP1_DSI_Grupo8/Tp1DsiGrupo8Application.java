1ba711cf3428bae23a06a9d2ee4252297d5aff84
mauroR5Helvecia <rosalesmauro369@gmail.com>
2024-11-18 16:10:55 -0300

    initial proyect spring boot with dependencies springDataJPA Spring web driverMysql

---
+++ src/main/java/com/G8/TP1_DSI_Grupo8/Tp1DsiGrupo8Application.java
@@ -0,0 +1,13 @@
+package com.G8.TP1_DSI_Grupo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1DsiGrupo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1DsiGrupo8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/G8/TP1_DSI_Grupo8/Tp1DsiGrupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G8.TP1_DSI_Grupo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1DsiGrupo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
